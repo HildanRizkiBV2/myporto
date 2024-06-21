9e8a391a9dc2d05ee1442e9cd7ac79366219fd4c
HildanRizkiBV2 <hildanrizki16@gmail.com>
2024-06-21 22:28:31 +0700

    fix display

--- src/pages/CekKodam.jsx
+++ src/pages/CekKodam.jsx
@@ -10,22 +10,7 @@ const CekKodam = () => {
       setName(name);
     }
   }, []);
-  const arr = [
-    "kodok",
-    "acumalaka",
-    "srigala hitam",
-    "sapu makam",
-    "batu ginjal",
-    "sapi jantan",
-    "kuda laut",
-    "macan kuntul",
-    "ambatron asli ngawi",
-    "kumar asli",
-    "hp meizu",
-    "redmi 9c",
-    "megachan",
-    "sigma skibidi",
-  ];
+  const arr = ["ratu iblis"];
 
   useEffect(() => {
     const kodamnya = arr[Math.floor(Math.random() * arr.length)];
@@ -37,15 +22,15 @@ const CekKodam = () => {
         onClick={() => window.history.back()}
         style={{ fontSize: "50px", fontWeight: "bold" }}
       />
-      <div className="cekkodam">
-        <h1>Cek kodam</h1>
-        <h3>di dalam diri {name} saya merasakan adanya </h3>
-        <h3>kodam</h3>
-        <h1>{kodam}</h1>
+      <div className="container-cekkodam">
+        <div className="cekkodam">
+          <h1>Cek kodam</h1>
+          <h3>di dalam diri {name} saya merasakan adanya </h3>
+          <h3>kodam</h3>
+          <h1>{kodam}</h1>
 
-        <h3>terima kasih</h3>
-
-        <span>JANGAN DI REFRESH WOEE!!!!!!!!!</span>
+          <h3>terima kasih</h3>
+        </div>
       </div>
     </div>
   );
